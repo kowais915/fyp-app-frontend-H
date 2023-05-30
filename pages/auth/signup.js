f2719b8942e96a5d5d244473da241c08b3afe383
kowais915 <kowais915@gmail.com>
2023-05-30 13:18:34 -0600

    :sparkles: Added a signup form

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,9 +1,27 @@
+import styles from '@/styles/Login.module.css'
 const Signup = () => {
     return ( 
 
         <>
-        
-            <h1>This is the sign up page.</h1>
+            <form className={styles.container}>
+                <h1 style={{"max-width":"600px", "display":"flex", "justifyContent":"center", "margin": "60px"}}>Register!</h1>
+
+                <div class="mb-3 row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                        <input type="email" class="form-control" id="inputPassword"/>
+                        </div>
+                </div>
+                
+                
+                <div class="mb-3 row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10">
+                        <input type="password" class="form-control" id="inputPassword"/>
+                        </div>
+                </div>
+
+            </form>
         </>
      );
 }
