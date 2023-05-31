af8f370fea2959985cd2657ee8fb8a5551accbcd
kowais915 <kowais915@gmail.com>
2023-05-31 21:42:47 +0500

    :lipstick: Styled the about page

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,9 @@
 const About = () => {
     return ( 
-        <>
-            <h1>This is the about page.</h1>
-        </>
+        <div className="about">
+            <h1>About</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis hic cumque similique, asperiores ut tempora id quidem quam at pariatur explicabo libero nisi alias quae consectetur iusto consequatur possimus vel quos placeat. Tempora sed quas perferendis aut enim! Fugiat, itaque.</p>
+        </div>
      );
 }
  
